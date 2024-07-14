dfb309e6284290bbabe1b7afbb3d098c47f9ba37
nithya1012 <nithya2002.kalidoss@gmail.com>
2024-07-14 21:21:40 +0530

    commit2

---
+++ Reverse.java
@@ -0,0 +1,14 @@
+class Reverse{
+    public static void main(String[]args){
+        int num=123;
+        // int rev=0;
+        while(num!=0){
+            int rem=num%10;
+            num=num/10;
+            // rev=rev*10+rem;
+            System.out.print(rem+"  ");
+        }
+        
+
+    }
+}
